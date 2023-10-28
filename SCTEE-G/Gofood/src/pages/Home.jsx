7cdcaa4ad8ef8ec24ad2ac642d61866cf63e71d3
Khairunnisa28 <138001140+Khairunnisa28@users.noreply.github.com>
2023-10-28 16:00:28 +0700

    Add files via upload

--- SCTEE-G/Gofood/src/pages/Home.jsx
+++ SCTEE-G/Gofood/src/pages/Home.jsx
@@ -7,7 +7,7 @@ import './Home.css';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function Home(){
     const initialProduct = [
@@ -17,8 +17,29 @@ function Home(){
         {id: 4, name: 'Udon', price: 55000, quantity: 0, image: Udon},
     ]
 
-    const [products, setProduct] = useState(initialProduct);
+    const [products, setProducts] = useState(initialProduct);
 
+    function increaseOrderCount(id) {
+        setProducts((prevProducts) => {
+          return prevProducts.map((product) => {
+            if (product.id === id) {
+              return { ...product, quantity: product.quantity + 1 };
+            }
+            return product;
+          });
+        });
+      }
+    
+      function decreaseOrderCount(id) {
+        setProducts((prevProducts) => {
+          return prevProducts.map((product) => {
+            if (product.id === id && product.quantity > 0) {
+              return { ...product, quantity: product.quantity - 1 };
+            }
+            return product;
+          });
+        });
+      }
     return(
         <div className="content">
             <img src={ImgHeader} className="img-header"/>
@@ -31,9 +52,21 @@ function Home(){
                         <h5 className='card-title'>{product.name}</h5>
                         <h6>Rp.{product.price}</h6>
                         <div className='count-btn'>
-                            <button className='btn btn-light'>-</button>
-                            <span>{product.quantity}</span>
-                            <button className='btn btn-light'>+</button>
+                            <button
+                            type="button"
+                            onClick={() => decreaseOrderCount(product.id)}
+                            className="btn btn-secondary"
+                            >
+                            -
+                            </button>
+                            {product.quantity}
+                            <button
+                            type="button"
+                            onClick={() => increaseOrderCount(product.id)}
+                            className="btn btn-secondary"
+                            >
+                            +
+                            </button>
                         </div>
                         <a href="#" className='btn btn-primary'>Order</a>
                     </div>
