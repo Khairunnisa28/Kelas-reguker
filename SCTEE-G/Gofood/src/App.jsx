ae1f2060075f332c2dc12c71003cebdc62dfdad4
Khairunnisa28 <138001140+Khairunnisa28@users.noreply.github.com>
2023-11-17 17:52:05 +0700

    Add files via upload

--- SCTEE-G/Gofood/src/App.jsx
+++ SCTEE-G/Gofood/src/App.jsx
@@ -1,45 +1,23 @@
-import React, { useState } from 'react';
+import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './component/Navbar';
+import Navbar from './component/navbar';
 import Home from './pages/Home';
-import Cart from './component/cart'
+import './App.css'
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-  const [cartVisible, setCartVisible] = useState(false);
-
-  const addToCart = (item) => {
-    setCartItems((prevItems) => {
-      const existingItem = prevItems.find((i) => i.id === item.id);
-      if (existingItem) {
-        existingItem.quantity += 1;
-        return [...prevItems];
-      } else {
-        return [...prevItems, { ...item, quantity: 1 }];
-      }
-    });
-  };
-
-  const openCart = () => {
-    setCartVisible(true);
-  };
-
-  const closeCart = () => {
-    setCartVisible(false);
-  };
-
   return (
     <BrowserRouter>
-      <div className="App">
-        <Navbar openCart={openCart} /> {/* Pass the openCart function to the Navbar */}
-        <div className="content">
+      <Navbar/>
+      <div className='App'>
+        <div className='content'>
           <Routes>
-            <Route path="/" element={<Home addToCart={addToCart} />} /> {/* Pass the addToCart function to Home */}
+            <Route path='/' element={<Home/>}/>
+            <Route path='/cart'/>
           </Routes>
         </div>
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- SCTEE-G/Gofood/src/component/navbar.jsx
+++ SCTEE-G/Gofood/src/component/navbar.jsx
@@ -1,26 +1,23 @@
-import React from "react";
-import Foto from "../image/logo.png";
-import "./Navbar.css";
-
+import Logo from '../image/Logo.png';
+import './navbar.css';
 
 function Navbar(){
     return(
-        <nav className="navbar">
-            <div className="logo">
-                <img src={Foto} alt="foto" />
+        <nav className='navbar'>
+            <div className='logo'>
+                <img src={Logo} alt='Logo'/>
             </div>
-            <ul className="nav-list">
-                <li className="nav-item">
+            <ul className='nav-list'>
+                <li className='nav-item'>
                     <a href="/">Home</a>
                 </li>
-                <li className="nav-item">
-                    <a href="/food">Recommendation</a>
+                <li className='nav-item'>
+                    <a href="/Product">Product</a>
                 </li>
-                <li className="nav-item">
-                    <a href="/Cart">Cart</a>
+                <li className='nav-item'>
+                    <a href="/contact">Contact</a>
                 </li>
             </ul>
-
         </nav>
     )
 }
